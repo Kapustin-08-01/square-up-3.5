6c60906b930f5743dd006b6dcaeb3a3f81c4885c
Aleksey Kapustin <jqsmw6@gmail.com>
2026-01-09 19:55:59 +0300

    feat: add assets pipeline for fonts and images

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ import htmlmin from 'gulp-htmlmin';
 import browserSyncModule from 'browser-sync';
 const browserSync = browserSyncModule.create();
 import del from 'del';
+import imagemin from 'gulp-imagemin';
 
 const paths = {
   src: 'src',
@@ -19,11 +20,14 @@ const paths = {
 const sourcePaths = {
   styles: `${paths.src}/scss/**/*.scss`,
   html: `${paths.src}/**/*.html`,
+  images: `${paths.src}/assets/images/**/*`,
+  fonts: `${paths.src}/assets/fonts/**/*`,
 };
 
 const destPaths = {
   styles: paths.dist,
   html: paths.dist,
+  assets: `${paths.dist}/assets`,
 };
 
 const styles = () => {
@@ -42,6 +46,19 @@ const html = () => {
     .pipe(browserSync.stream());
 };
 
+const images = () => {
+  return src(sourcePaths.images)
+    .pipe(imagemin())
+    .pipe(dest(destPaths.assets))
+    .pipe(browserSync.stream());
+};
+
+const fonts = () => {
+  return src(sourcePaths.fonts)
+    .pipe(dest(destPaths.assets))
+    .pipe(browserSync.stream());
+};
+
 const clean = () => del([paths.dist]);
 
 const serve = () => {
@@ -52,8 +69,10 @@ const serve = () => {
   });
   watch(sourcePaths.styles, styles);
   watch(sourcePaths.html, html);
+  watch(sourcePaths.images, images);
+  watch(sourcePaths.fonts, fonts);
 };
 
-export default series(clean, parallel(styles, html), serve);
-export const build = series(clean, parallel(styles, html));
-export { styles, html, clean, serve };
+export default series(clean, parallel(styles, html, images, fonts), serve);
+export const build = series(clean, parallel(styles, html, images, fonts));
+export { styles, html, images, fonts, clean, serve };
